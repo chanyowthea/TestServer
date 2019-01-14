6413b7314fd1f20775e569849be8508e4b395489
chanyowthea <454982165@qq.com>
2019-01-14 23:09:50 +0800

    Update Program.cs

--- TestClient/TestClient/Program.cs
+++ TestClient/TestClient/Program.cs
@@ -18,12 +18,13 @@ namespace TestClient
             SocketTcpClient client = new SocketTcpClient();
             client.Start(); 
             client.Connect();
-            client.SendMessage(Encoding.UTF8.GetString(new byte[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
-            client.SendMessage(Encoding.UTF8.GetString(new byte[] { 20, 21, 22 }));
-            client.SendMessage(Encoding.UTF8.GetString(new byte[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 }));
+            //client.SendMessage(Encoding.UTF8.GetString(new byte[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+            //client.SendMessage(Encoding.UTF8.GetString(new byte[] { 20, 21, 22 }));
+            //client.SendMessage(Encoding.UTF8.GetString(new byte[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 }));
 
-            //client.SendMessage("kitty is pretty! ");
-            //client.SendMessage("i live in a beautiful mountain! ");
+            client.SendMessage("kitty is pretty! ");
+            client.SendMessage("a cute cat seat on the table! ");
+            client.SendMessage("i live in a beautiful mountain! ");
             Console.ReadKey(); 
         }
     }
